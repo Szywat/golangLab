3d7497b68db6a93b5b24f3987c715002bbd2fafd
szywat <szywat04@gmail.com>
2025-05-28 21:14:35 +0200

    added float64 and shortened counting

--- lab06/trend.go
+++ lab06/trend.go
@@ -7,18 +7,17 @@ import (
 
 func EMA(data []structures.Data) float64 {
 	n := len(data)
-	alfa := 2 / (n + 1)
+	alfa := 2.0 / float64(n + 1)
 
-	var ema float64
 	var numerator float64
 	var denominator float64
 
 	for i := range n {
-		numerator = numerator + data[n-i-1].Last*math.Pow(float64(1-alfa), float64(i))
-		denominator = denominator + math.Pow(float64(1-alfa), float64(i))
+		numerator += data[n-i-1].Last*math.Pow(1-alfa, float64(i))
+		denominator += + math.Pow(1-alfa, float64(i))
 	}
 
-	ema = numerator / denominator
+	ema := numerator / denominator
 
 	return ema
 
